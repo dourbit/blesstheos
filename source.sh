7faa68dc3c97c0d4ea87d0a79de119b5df9ffe8c
Orlin M Bozhinov <om@soundsapiens.com>
2015-02-06 17:47:57 +0200

    bin and better PATH(s)

--- source.sh
+++ source.sh
@@ -1,12 +1,15 @@
 ## /usr/bin/env source $(dirname $0)/$(basename $0)
 
+# NOTE: using $DOTS_HOME instead of the $HOME_DOTS used to set it!
+if [ -z $HOME_DOTS ]; then
+  export DOTS_HOME="$(dirname $0)"
+else
+  # TODO: absolute path could be alowed - just check if it starts with /
+  export DOTS_HOME="$HOME/$HOME_DOTS"
+fi
+
 function uses {
-  if [ -z $HOME_DOTS ]; then
-    use="$(dirname $0)/use/$1"
-  else
-    use="$HOME/$HOME_DOTS/use/$1"
-  fi
-  source $use
+  source "$DOTS_HOME/use/$1"
 }
 
 uses colors.sh
@@ -18,9 +21,9 @@ uses git.sh # also improves the prompt in non-git contexts
 umask 022
 
 export NODE_ENV=development
-
 export JAVA_HOME=`/usr/libexec/java_home`
-export PATH=$PATH:${JAVA_HOME}
+
+export PATH=${PATH}:${JAVA_HOME}:${DOTS_HOME}/bin
 
 export GREP_COLOR='1;32' # why is it different than what colors.sh exports?
 export GREP_OPTIONS='-i --color=auto' # case-insensitive
