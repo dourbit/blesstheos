698c7042e5c7269c51846bd0f39e5c243e6781a6
Orlin M Bozhinov <om@soundsapiens.com>
2015-02-07 10:18:57 +0200

    a note to reseatch inputrc options

--- use/bash.sh
+++ use/bash.sh
@@ -1,6 +1,7 @@
 # bash completion settings (actually, these are readline settings)
 # NOTE: bind used instead of sticking these in .inputrc
 
-bind "set completion-ignore-case on"
 bind "set bell-style none" # no bell
-bind "set show-all-if-ambiguous On" # show list automatically, without double tab
+bind "set completion-ignore-case on" # case-insensitive tab-completion
+bind "set show-all-if-ambiguous On" # show tab-completion list with a single tab
+# anything else? http://ss64.com/bash/syntax-inputrc.html
