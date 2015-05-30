c41141bbe5dde16be721ea11d077590d22849846
Orlin M Bozhinov <om@soundsapiens.com>
2015-05-30 17:33:01 +0300

    there seems to be something wrong with 14  - cd PATH - noticed because it got to be 'too long'

--- source.sh
+++ source.sh
@@ -11,8 +11,9 @@ fi
 # http://unix.stackexchange.com/questions/4965/keep-duplicates-out-of-path-on-source
 add_to_PATH () {
   for d; do
-    d=$(cd -- "$d" && { pwd -P || pwd; }) 2>/dev/null  # canonicalize symbolic links
-    if [ -z "$d" ]; then continue; fi  # skip nonexistent directory
+    # d=$(cd -- "$d" && { pwd -P || pwd; }) 2>/dev/null  # canonicalize symbolic links
+    # if [ -z "$d" ]; then continue; fi  # skip nonexistent directory
+    if ! [ -d "$d" ]; then continue; fi
     case ":$PATH:" in
       *":$d:"*) :;;
       *) PATH=$PATH:$d;;
