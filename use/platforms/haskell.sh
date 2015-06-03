606731f3f3aa59b74beb7869a3cbfebfb675517a
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-03 10:16:27 +0300

    setting up haskell, not so good...

--- use/platforms/haskell.sh
+++ use/platforms/haskell.sh
@@ -8,4 +8,8 @@ fi
 add_to_PATH ~/.cabal/bin
 
 # https://halcyon.sh/
-eval "$( HALCYON_NO_SELF_UPDATE=1 "/app/halcyon/halcyon" paths )"
+# I'd like to use the above, however there are some issues with it...
+# https://github.com/mietek/halcyon/issues/23
+# https://github.com/mietek/halcyon/issues/16
+# When it's all good - skip the ghcformacosx.github.io in favor of halcyon.
+# eval "$( HALCYON_NO_SELF_UPDATE=1 "/app/halcyon/halcyon" paths )"
