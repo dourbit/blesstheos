2cc9dce705201ee74700e31563622b515ba5c3df
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-03 09:17:51 +0300

    apm aliases

--- use/aliases.sh
+++ use/aliases.sh
@@ -6,12 +6,13 @@ alias f='find . -iname'
 alias ducks='du -cksh * | sort -rn|head -11' # file/dir sizes of the current dir
 alias systail='tail -f /var/log/system.log'
 alias less='less -R'
-alias m='less -R' # less is more -- would it pick the -R from the less alias?
+alias more='less -R' # less is more -- would it pick the -R from the less alias?
 alias df='df -h'
 
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal-274317.php
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
+uses aliases/apm.sh
 uses aliases/npm.sh
 uses aliases/pip.sh
 
---
+++ use/aliases/apm.sh
@@ -0,0 +1,3 @@
+alias als='apm list --installed --bare --links'
+alias als-up='apm upgrade -l'
+alias apm-up-all='apm upgrade --no-confirm'
