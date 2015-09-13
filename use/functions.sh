1b5b44abd2a648e2188ac2c323ea5bbcd61ad10d
Orlin M Bozhinov <om@soundsapiens.com>
2015-09-13 15:00:35 +0300

    tre-tracker uri | tre-info

--- use/functions.sh
+++ use/functions.sh
@@ -8,12 +8,13 @@ fi
 
 function tre-ids-pipe {
   # give it a function or script to call
+  # pass on the rest of the args (slice)
   if [[ -p /dev/stdin ]]; then
     # the input is coming from a pipe
-    eval $1
+    eval $1 "${@:2}"
   else
     # the stdbuf tip came from http://unix.stackexchange.com/questions/117501/in-bash-script-how-to-capture-stdout-line-by-line
-    stdbuf -oL tre-ids | eval $1
+    stdbuf -oL tre-ids | eval $1 "${@:2}"
   fi
 }
 export -f tre-ids-pipe
