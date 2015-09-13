98c47861ac44893cf2eb319bfb9e59565c559051
Orlin M Bozhinov <om@soundsapiens.com>
2015-09-13 19:31:24 +0300

    because export PATH="/Users/om/.rbenv/shims:${PATH}" source "/usr/local/Cellar/rbenv/0.4.0/libexec/../completions/rbenv.bash" rbenv rehash 2>/dev/null rbenv() {   typeset command   command="$1"   if [ "$#" -gt 0 ]; then     shift   fi
      case "$command" in
      rehash|shell)
        eval `rbenv "sh-$command" "$@"`;;
      *)
        command rbenv "$command" "$@";;
      esac
    } adds to my PATH each time I source ~/.bashrc

--- use/platforms/ruby.sh
+++ use/platforms/ruby.sh
@@ -1,7 +1,7 @@
 # https://github.com/sstephenson/rbenv
 
-# enable shims and autocompletion
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+# enable shims and autocompletion using .bash_profile
+# if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 # use the following, instead of ~/.rbenv
 # export RBENV_ROOT=/usr/local/var/rbenv
