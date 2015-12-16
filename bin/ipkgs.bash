f9ff56d929b82c876c2a1338e69c0ed4714bc939
Orlin M Bozhinov <om@soundsapiens.com>
2015-12-16 18:28:43 +0200

    introducing ipkgs-vars.bash - making the expected global vars easier to find

--- bin/ipkgs.bash
+++ bin/ipkgs.bash
@@ -75,26 +75,3 @@ items_init() {
 
   # echo "${#items[@]}"
 }
-
-# special cases when command isn't the filename
-# if a file isn't in this list, the $command is taken up until the first dash
-declare -A special=(
-  ["apt"]="apt-get"
-  ["apt-get"]="apt-get" # just for example, or the '-get' would get truncated
-  ["brew-cask"]="brew cask"
-)
-
-# some commands can only run on certain operating systems
-# these are confirmed by filename
-system=`uname -s`
-declare -A syscheck=(
-  ["apm-darwin"]="Darwin"
-  ["apt"]="Linux" # could check for ubuntu or debian, yet presence is enough
-  ["brew"]="Darwin"
-  ["brew-cask"]="Darwin"
-)
-
-# it's `$pm install $package` - except for the following special cases
-declare -A install=(
-  ["npm"]="npm i -g"
-)
