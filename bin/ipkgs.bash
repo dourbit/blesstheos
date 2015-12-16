0b93b4f0d67aa3dfacd8246a4ab3a5a6909ee443
Orlin M Bozhinov <om@soundsapiens.com>
2015-12-16 17:38:15 +0200

    ready to add more options, all validation code has moved to ipkgs.bash

--- bin/ipkgs.bash
+++ bin/ipkgs.bash
@@ -12,6 +12,56 @@ usage() {
   echo "Usage: $(basename $0) [-p] file(s)"
 }
 
+options=()
+arguments=()
+
+for arg in "$@"; do
+  if [ "${arg:0:1}" = "-" ]; then
+    if [ "${arg:1:1}" = "-" ]; then
+      options[${#options[*]}]="${arg:2}"
+    else
+      index=1
+      while option="${arg:$index:1}"; do
+        [ -n "$option" ] || break
+        options[${#options[*]}]="$option"
+        let index+=1
+      done
+    fi
+  else
+    arguments[${#arguments[*]}]="$arg"
+  fi
+done
+
+if [ "${#arguments[@]}" -eq 0 ]; then
+  usage >&2
+  exit 1
+fi
+
+unset paths
+
+for option in "${options[@]}"; do
+  case "$option" in
+  "p" | "paths" )
+    paths="1"
+    ;;
+  * )
+    usage >&2
+    exit 1
+    ;;
+  esac
+done
+
+declare where # a path prefix
+if [ -n "$paths" ]; then
+  # the --paths flag, meaning...
+  # valid paths provided as part of the files - either relative or absolute
+  where='' # no need to adjust location
+else
+  # less typing - uses env var or the relative default
+  echo Using \$IPKGS_PATH = \'${IPKGS_PATH:=$(realpath "$(dirname $0)/../install/packages")}\'.
+  where="$IPKGS_PATH/"
+fi
+
 # special cases when command isn't the filename
 # if a file isn't in this list, the $command is taken up until the first dash
 declare -A special=(
