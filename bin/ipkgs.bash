1ce16507659c3698afe8d0e795d6a090e3d89672
Orlin M Bozhinov <om@soundsapiens.com>
2015-12-16 16:45:13 +0200

    move more code to ipkgs.bash - to remain as is

--- bin/ipkgs.bash
+++ bin/ipkgs.bash
@@ -7,3 +7,30 @@ if [ ${BASH_VERSION%%[^0-9]*} -lt 4 ]; then
   echo "Currently it's: '${BASH_VERSION}'."
   exit 1
 fi
+
+usage() {
+  echo "Usage: $(basename $0) [-p] file(s)"
+}
+
+# special cases when command isn't the filename
+# if a file isn't in this list, the $command is taken up until the first dash
+declare -A special=(
+  ["apt"]="apt-get"
+  ["apt-get"]="apt-get" # just for example, or the '-get' would get truncated
+  ["brew-cask"]="brew cask"
+)
+
+# some commands can only run on certain operating systems
+# these are confirmed by filename
+system=`uname -s`
+declare -A syscheck=(
+  ["apm-darwin"]="Darwin"
+  ["apt"]="Linux" # could check for ubuntu or debian, yet presence is enough
+  ["brew"]="Darwin"
+  ["brew-cask"]="Darwin"
+)
+
+# it's `$pm install $package` - except for the following special cases
+declare -A install=(
+  ["npm"]="npm i -g"
+)
