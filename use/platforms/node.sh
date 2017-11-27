b02cd38a23c523421a19ba4ad2882762f69286ac
Orlin M Bozhinov <orlin.monad@gmail.com>
2017-11-27 22:00:59 +0200

    .nvm/bash_completion

--- use/platforms/node.sh
+++ use/platforms/node.sh
@@ -1,6 +1,9 @@
 export NVM_DIR=~/.nvm
 system_name=`uname -s`
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 if [ $system_name == 'Darwin' ]; then
   source $(brew --prefix nvm)/nvm.sh
 else
