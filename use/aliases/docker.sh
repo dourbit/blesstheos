5b4bce97966f6bd16fa7e1dec130656060af4425
Orlin M Bozhinov <orlin@apotelesmatic.com>
2019-08-31 16:55:27 +0300

    docker-ids --no-stream and docker-all

--- use/aliases/docker.sh
+++ use/aliases/docker.sh
@@ -1,6 +1,7 @@
 # https://docs.docker.com/engine/reference/commandline/stats/
-alias docker-id='docker stats --format "table {{.PIDs}}\t{{.Container}}\t{{.Name}}"'
 alias docker-stats='docker stats --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}"'
+alias docker-ids='docker stats --format "table {{.PIDs}}\t{{.Container}}\t{{.Name}}" --no-stream'
+alias docker-all='docker-ids --all'
 
 # Container Shortcuts
 alias siac='docker exec -it sia ./siac'
