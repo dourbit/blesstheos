dc1a914c07581b4ee2f8ab8d479d89c3667f0fd0
Orlin M Bozhinov <orlin@apotelesmatic.com>
2019-09-02 16:15:36 +0300

    correct bitcoind mainnet ports

--- use/aliases/docker.sh
+++ use/aliases/docker.sh
@@ -4,6 +4,7 @@ alias docker-ids='docker stats --format "table {{.PIDs}}\t{{.Container}}\t{{.Nam
 alias docker-all='docker-ids --all'
 
 # Container Shortcuts
+alias bitcoind='docker exec -it btcnode bitcoind -datadir=/.bitcoin'
 alias bitcoin-cli='docker exec -it btcnode bitcoin-cli -datadir=/.bitcoin'
 alias bitcoin-log='sudo tail -f ~/data/btc/debug.log'
 alias siac='docker exec -it sia ./siac'
