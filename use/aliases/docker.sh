23fc2ff9ffca512d7c949d83ea67b03eb7baf8bf
Orlin M Bozhinov <orlin@apotelesmatic.com>
2019-09-09 13:55:23 +0300

    btcblocks seems like a better alias

--- use/aliases/docker.sh
+++ use/aliases/docker.sh
@@ -8,7 +8,7 @@ alias docker-all='docker-ids --all'
 alias bitcoind='docker exec -it btcnode bitcoind -datadir=/.bitcoin'
 alias bitcoin-cli='docker exec -it btcnode bitcoin-cli -datadir=/.bitcoin'
 alias bitcoin-log='sudo tail -f ~/data/btc/debug.log'
-alias btcblock='BC_CURRENT=`bitcoin-cli getblockcount 2>&1`; BC_HEIGHT=`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`; perl -E "say sprintf(\"Block %s of %s (%.2f%%)\", $BC_CURRENT, $BC_HEIGHT, ($BC_CURRENT/$BC_HEIGHT)*100)";'
+alias btcblocks='BC_CURRENT=`bitcoin-cli getblockcount 2>&1`; BC_HEIGHT=`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`; perl -E "say sprintf(\"Block %s of %s (%.2f%%)\", $BC_CURRENT, $BC_HEIGHT, ($BC_CURRENT/$BC_HEIGHT)*100)";'
 alias bc='bitcoin-cli'
 alias bd='bitcoind'
 # sia
