36d4adc817f882886c6dd2110aee9ff667a04ed7
Orlin M Bozhinov <orlin@apotelesmatic.com>
2020-03-12 13:00:17 +0200

    vultr

---
+++ cloud/vultr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# TODO: add docker-machine parameter for v0.16.0
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
+base=https://raw.githubusercontent.com/docker/machine/v0.16.0
+for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
+do
+  sudo wget "$base/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+done
+
+source /etc/bash_completion.d/docker-machine-prompt.bash
