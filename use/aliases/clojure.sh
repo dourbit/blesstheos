43bb7c4e3d3e706cd8c3de9fc1d25d76613a08c4
Orlin M Bozhinov <om@soundsapiens.com>
2020-05-05 17:21:47 +0300

    closh-zero.jar via install/clojure + alias as closh

--- use/aliases/clojure.sh
+++ use/aliases/clojure.sh
@@ -1,3 +1,7 @@
+# Upgrades
 alias lein-up='sudo lein upgrade'
 alias clojure-up='brew update && brew upgrade clojure/tools/clojure'
 alias clj-up='clojure-up'
+
+# Clojure for bash-like scripts
+alias closh='closh-zero.jar'
