d9ecaeee87d0befd01855ee507acf625401a1316
Orlin M Bozhinov <om@soundsapiens.com>
2020-05-21 13:30:56 +0300

    df # no longer annoyingly long stuff

--- use/aliases.sh
+++ use/aliases.sh
@@ -2,13 +2,13 @@ alias ..='cd ..'
 alias ...='cd .. ; cd ..'
 
 alias please=sudo
+alias df='df -h -x squashfs -x tmpfs -x devtmpfs'
 alias grep='grep -i --color=auto' # case-insensitive
 alias f='find . -iname'
 alias ducks='du -cksh * | sort -rn|head -11' # file/dir sizes of the current dir
 alias systail='tail -f /var/log/system.log'
 alias less='less -R'
 alias more='less -R' # less is more -- would it pick the -R from the less alias?
-alias df='df -h'
 
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal-274317.php
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
