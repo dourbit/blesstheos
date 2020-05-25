02c2dc4fc4042e749bc7790aaec3fc7e1c51335b
Orlin M Bozhinov <om@soundsapiens.com>
2020-05-25 22:43:35 +0300

    move check-x into its own bin-fn/

--- use/helpers/path.sh
+++ use/helpers/path.sh
@@ -16,20 +16,3 @@ add_to_PATH() {
   done
 }
 export -f add_to_PATH
-
-# check that a command or function or alias exists
-# thus conclude that something is installed
-# only missing -x check for the executables, which would depend on type
-check-x() {
-  if [ $# -eq 0 ]; then
-    echo "Usage: check-x <command>"
-    false; return
-  elif [ -z "$(command -v $1)" ]; then
-    # Empty string of command -v (is good enough)
-    # Don't check for ! [ -x ...] because that doesn't catch a function or alias
-    echo
-    echo "Command '$1' not found."
-    false; return
-  fi
-}
-export -f check-x
