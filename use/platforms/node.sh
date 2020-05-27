78bd85b7f532153a13c09d5b303a2d8e56d4944c
Orlin M Bozhinov <om@soundsapiens.com>
2020-05-27 15:50:10 +0300

    uses platforms/node.sh may be called before yarn is installed

--- use/platforms/node.sh
+++ use/platforms/node.sh
@@ -3,14 +3,12 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-if onMac; then
-  . $(brew --prefix nvm)/nvm.sh
-fi
-
 if onLinux; then
-  [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
+  [ -s "$NVM_DIR"/nvm.sh ] && . "$NVM_DIR"/nvm.sh
+elif onMac; then
+  . "$(brew --prefix nvm)"/nvm.sh
 fi
 
 # https://github.com/yarnpkg/yarn/issues/5353
 # after the nvm / npm global packages
-add_to_PATH "$(yarn global bin)"
+check-x yarn && add_to_PATH "$(yarn global bin)"
