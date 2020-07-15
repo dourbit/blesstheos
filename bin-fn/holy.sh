ca3e753ac40c4fc1a5649469565ccb11979d44e9
Orlin M Bozhinov <om@soundsapiens.com>
2020-07-15 12:24:18 +0300

    holy refactor

---
+++ bin-fn/holy.sh
@@ -0,0 +1,15 @@
+installable() {
+  local able=$1
+  local path="${HOLY_HOME}/install/${able}"
+  if [ $# -eq 0 ]; then
+    find $HOLY_HOME/install -type f,l -executable -print \
+    | sed -e "s/^.*\/install\///g" | sort \
+    | grep -ve '\.sh$\|-skip$'
+    # TODO: show help regarding what the above installs?
+    true; return
+  elif check-x $path; then
+    true; return
+  else
+    false; return
+  fi
+}
