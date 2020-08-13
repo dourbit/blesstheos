6ac898243e831d200fd18ac428a91535ec277c93
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-13 20:51:02 +0300

    init/once.sh does even more; document it

--- init/once.sh
+++ init/once.sh
@@ -1,3 +1,16 @@
+# Requires two env vars:
+# 1. $HOLY_GOAL
+# 2. $HOLY_HERE
+# 3. $HOLY_COPY - becomes a comment in the $HOLY_HERE config (optional)
+# TODO: add checks to make it official
+
+# Does:
+# failsafe a first-time init (hence "once") - unless forced
+# export the $HOLY_GOAL path, without requiring that a path be explicitly given
+# append the $HOLY_GOAL to $HOLY_HERE, starting with a # $HOLY_COPY
+# ensure the $HOLY_HERE remains unchanges unless all is as expected
+
+
 set -eE # exit on error
 trap 'echo "Did not init $(basename $0)!"' ERR
 
@@ -13,4 +26,13 @@ fi
 ${path}/once  $HOLY_GOAL $HOLY_HERE "$@"
 home=$(${path}/home $HOLY_GOAL $(dirname $0)/..)
 
+
 export $HOLY_GOAL="$home"
+
+touch  $HOLY_HERE
+tee -a $HOLY_HERE > /dev/null << END
+
+
+# $HOLY_COPY
+export $HOLY_GOAL="${!HOLY_GOAL}"
+END
