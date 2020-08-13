5d9a2d43620c53f7c5b7848291cb851e47089e60
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-13 19:58:49 +0300

    holy you init ...

--- init/once.sh
+++ init/once.sh
@@ -1,5 +1,16 @@
 set -eE # exit on error
 trap 'echo "Did not init $(basename $0)!"' ERR
-$(dirname $0)/once $HOLY_GOAL $HOLY_HERE "$@"
 
-export $HOLY_GOAL=$($(dirname $0)/home)
+if [ ${HOLY_GOAL} == "HOLY_HOME" ]; then
+  path=$(dirname $0)
+else
+  # NOTE: here we have holy & all its helper functions guaranteed by holy init
+  # though we don't need much from it
+  path=$(holy one path init)
+fi
+
+# NOTE: init/once & init/home reused by everyone
+${path}/once  $HOLY_GOAL $HOLY_HERE "$@"
+home=$(${path}/home $HOLY_GOAL $(dirname $0)/..)
+
+export $HOLY_GOAL="$home"
