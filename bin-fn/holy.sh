4d819ed82baa4a1431173bf74cc649a75205183e
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-15 18:37:11 +0300

    holy call + refactored exception list for holy-on req + a goal fn

--- bin-fn/holy.sh
+++ bin-fn/holy.sh
@@ -1,16 +1,33 @@
 # Helpers for holy
 
-# validates a lead modifier
+# validates a lead mod aka modifier
 lead() {
   [[ "$1" == "one" || "$1" == "you" ]] && true || false
 }
 
+# return the holy var name of a mod with which to get its home path
+# if unknown mod then echo a "HOLY_HOME" default
+goal() {
+  local the=${1-one}; shift
+  case $the in
+    you)
+      echo DOTS_HOME
+      ;;
+    one|*)
+      echo HOLY_HOME
+  esac
+}
+
+# the rc file for a shell; to use by holy init; relative to ~/.
 shell-rc() {
   local shell=${1-$(basename $SHELL)}
   if [ $shell == "bash" ]; then
     echo ".bashrc"
   fi
 }
+
+
+# The rest of these require a holy one on.
 
 installable() {
   local able=$1
