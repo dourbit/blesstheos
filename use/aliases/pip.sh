e00b251bed1b8ea02245fb1e7c6154064ba0d612
Orlin M Bozhinov <om@soundsapiens.com>
2020-11-30 23:20:40 +0200

    python3 means pip3; pip-review needed; holy in platform/python

--- use/aliases/pip.sh
+++ use/aliases/pip.sh
@@ -1,10 +1,11 @@
 # pip's upgrading of packages is unintuitive
 # these are my aliases, a kind of cheatsheet
 
-alias pip-iv="pip freeze | cut -d = -f 1 | xargs -n 1 pip search | grep -B2 'LATEST:'"
+alias pip=pip3
+alias pip-iv="pip3 freeze | cut -d = -f 1 | xargs -n 1 pip3 search | grep -B2 'LATEST:'"
 alias pip-uv="pip-review"
-alias pip-up="sudo pip install --upgrade"
 alias pip-ui="sudo pip-review --interactive"
+alias pip-up="sudo pip3 install --upgrade"
 alias pip-up-all="sudo pip-review --auto"
 
 
