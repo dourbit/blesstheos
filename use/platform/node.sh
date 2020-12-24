b1f39cd22746219aa64d6d916c3f1afd51ca309d
Orlin M Bozhinov <om@soundsapiens.com>
2020-12-24 18:30:52 +0200

    improved use/platform/node.sh

--- use/platform/node.sh
+++ use/platform/node.sh
@@ -3,17 +3,24 @@
 # use/aliases/npm.sh
 # src/node.sh
 
-holy-dot src/os
+holy-dot src/ install os
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+if holy-be-on platform/node; then
 
-if onLinux; then
-  [ -s "$NVM_DIR"/nvm.sh ] && . "$NVM_DIR"/nvm.sh
-elif onMac; then
-  . "$(brew --prefix nvm)"/nvm.sh
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+
+  if onLinux; then
+    [ -s "$NVM_DIR"/nvm.sh ] && . "$NVM_DIR"/nvm.sh
+  elif onMac; then
+    . "$(brew --prefix nvm)"/nvm.sh
+  fi
+
+  # silently use /home/orlin/.nvmrc (this will pick up a change)
+  nvm use > /dev/null
+
+  # https://github.com/yarnpkg/yarn/issues/5353
+  # after the nvm / npm global packages
+  [ -x "$(command -v yarn)" ] && PATH-add "$(yarn global bin)"
+
 fi
-
-# https://github.com/yarnpkg/yarn/issues/5353
-# after the nvm / npm global packages
-[ -x "$(command -v yarn)" ] && PATH-add "$(yarn global bin)"
