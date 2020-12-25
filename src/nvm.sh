6695d55468124855680601301a16d7050297ef35
Orlin M Bozhinov <om@soundsapiens.com>
2020-12-25 20:15:20 +0200

    nvm-dir and nvm-on implementation

--- src/nvm.sh
+++ src/nvm.sh
@@ -1,23 +1,31 @@
 holy-dot src/os
 
-# need the following for using nvm in subshell scripts, and more...
+nvm-dir() {
+  local dir
+  if tis-some $NVM_DIR; then
+    dir="$NVM_DIR"
+  elif onMac && brewOn; then
+    dir="$(brew --prefix nvm)"
+  else
+    # Linux or other
+    dir="$HOME"/.nvm
+  fi
+  # validate nvm directory
+  if [ -s "$dir"/nvm.sh ]; then
+    echo "$dir" && true; return
+  else
+    false; return
+  fi
+}
 
 nvm-on() {
-  # TODO: first set $NVM_DIR if it exists depending on the os ...
-  # then load "$NVM_DIR"/nvm.sh if it exists regardless of the os
-  # return status by finally checking command -v nvm > /dev/null
-  # use this nvm-on directly in the if of on/confirm/platform/node
-  # does the optimization remain as a consideration (keep the NOTE)?
-  # maybe an nvm-dir function will do it / be exactly enough?
-  # which would also use and verify $NVM_DIR - if already set
-  # then simply source $(nvm-dir) in this one + handle return codes
-  # also export $NVM_DIR with this function - rather than nvm-dir
-
-  export NVM_DIR="$HOME/.nvm"
-
-  if onLinux; then
-    [ -s "$NVM_DIR"/nvm.sh ] && . "$NVM_DIR"/nvm.sh
-  elif onMac; then
-    . "$(brew --prefix nvm)"/nvm.sh
+  local dir="$(nvm-dir)"
+  if [ -d "$dir" ]; then
+    export NVM_DIR="$dir"
+    . "$NVM_DIR"/nvm.sh
+    command -v nvm > /dev/null
+  else
+    >&2 echo "Not Found: nvm"
+    false; return
   fi
 }
