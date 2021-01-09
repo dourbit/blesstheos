f163e5657c68779d82d610c745dc65729019d872
Orlin M Bozhinov <om@soundsapiens.com>
2021-01-09 19:52:53 +0200

    install platform/ruby doesn't append to .bashrc

--- use/platform/ruby.sh
+++ use/platform/ruby.sh
@@ -8,6 +8,12 @@
 
 if silent holy on platform/ruby; then
 
+  if [ -d "$HOME/.rbenv" ]; then
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+    eval "$(rbenv init -)"
+  fi
+
   if tis-true $HOLY_ALIASES; then
     alias bex="bundle exec"
   fi
