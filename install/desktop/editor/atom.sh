5ab350082c2922a6c775e37034a6d6e8d732c20b
Orlin M Bozhinov <om@soundsapiens.com>
2021-01-12 20:10:22 +0200

    holy in desktop/editor/atom #missed

--- install/desktop/editor/atom.sh
+++ install/desktop/editor/atom.sh
@@ -3,7 +3,7 @@
 # atom.sh because atom won't use syntax highlighting, I guess taken for markup
 
 # Depends on aton
-# Using: onApt; sudoUse; sudo-apt-update; noInstall
+# Using: onApt sudoUse sudo-add-apt-repository sudo-apt-update noInstall
 holy-dot src/ os install util
 status=0
 
@@ -12,7 +12,7 @@ check-x atom && echo "Notice: atom is already installed, proceeding anyway"
 if onApt; then
   sudoUse
   wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-  sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+  sudo-add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
   sudo-apt-update
   sudo apt install -y atom || noInstall "atom"
 else
